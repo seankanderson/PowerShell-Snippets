952cae875822ce3a8ba85d722ed87522b1c89008
unknown <sean.anderson@datavirtue.com>
2017-11-11 14:11:18 -0500

    Initial Commit

---
+++ ExchangeWebServices/EwsSnippets.ps1
@@ -0,0 +1,59 @@
+﻿<# 
+    Access Microsoft Exchange / Office365 / Outlook.com folders in PowerShell
+
+    This script utilizes the EWS module from Powershell Gallery.
+    
+    https://www.powershellgallery.com/profiles/bartekb/
+
+    This example was written by Sean Anderson (http://seananderson.us)
+
+#>
+
+
+<# You need admin rights on your computer to install this module #> 
+Install-Module -Name EWS
+
+<# 
+    Reference assemblies directly
+    
+    You can access any .NET assembly from Powershell.  Custom modules typically wrap spcific .NET assemblies to simplify scripting.
+    The module used in this example (EWS) wraps Microsoft.Exchange.WebServices.Data with each cmdlet wrapping a certain class.
+    Referencing a class directly is referred to as the "old way" when a particular .NET library already has a powershell module wrapper available.  
+    Sometimes you have to use "the old way" if a cmdlet is not feature-complete or you need more control.
+   
+
+    $service = [Microsoft.Exchange.WebServices.Data.ExchangeService]::new()
+    $service.Url = 'https://outlook.office365.com/EWS/Exchange.asmx'
+    $service.Credentials = [System.Net.NetworkCredential]::new('sean.anderson@datavirtue.com', 'mypassword')
+
+#>
+
+#Connect-EWSService wraps [Microsoft.Exchange.WebServices.Data.ExchangeService]
+$service = $(Connect-EWSService -Mailbox sean.anderson@datavirtue.com `
+                                -ServiceUrl https://outlook.office365.com/EWS/Exchange.asmx `
+                                -Credential sean.anderson@datavirtue.com)
+
+#$service.Url 
+
+
+<#
+    List 10 emails from the Inbox with the word "amazon" in the subject.
+    Filter uses AQL syntax - https://msdn.microsoft.com/en-us/library/office/dn579420(v=exchg.150).aspx
+#>
+Get-EwsItem -Name Inbox -Filter subject:Amazon -Limit 10 | Format-Table
+
+
+<#
+    List all calendar entries with the words "interview with" in the subject.
+#>
+Get-EwsItem -Name Calendar -Filter subject:"interview way" | Format-Table
+
+
+<#
+    Add an appointment.
+#>
+New-EWSAppointment -Subject Test -Body 'A test meeting' -Location 'Home' -Start (Get-Date).AddDays(1) -Duration 0:30:0
+
+<#
+    More examples at https://github.com/bielawb/EWS/tree/master/docs
+#>
---
+++ ExchangeWebServices/README.md
@@ -0,0 +1,3 @@
+# PowerShell-Snippets
+
+I found this PowerShell module and spent a few hours troubleshooting all of the issues to create a working example. Understanding EWS error messages takes patience and knack for reading goat entrails. This script should have you building solutions quickly without much fuss.
