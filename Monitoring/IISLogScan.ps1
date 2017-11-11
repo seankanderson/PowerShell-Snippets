94e2a00fc3accea3ee73a823c96fac023abf8d2e
unknown <sean.anderson@datavirtue.com>
2017-11-11 17:46:13 -0500

    Adding monitoring scripts

---
+++ Monitoring/IISLogScan.ps1
@@ -0,0 +1,47 @@
+﻿<#
+	Sean K. Anderson / seananderson.us / 2014
+	
+	Point this script at an IIS log file and it will count the number of requests that exceed a certain response time in milliseconds.
+	
+	I used a variation of this script as a poor man's application monitoring script to warn the team when certain critical APIs were not 
+	responding to requests fast enough.  In this example I have 3875 (milliseconds, or 3.875 seconds) hard coded.  
+	
+	You could re-work this to accept parameters and run on intervals to send an email or text message if an API/website is running slow.  
+	
+#>
+
+
+$total = 0
+$long = 0
+
+select-string -path "\\cb-prodweb04\e$\IIS LOGS\W3SVC7\u_ex150204.log" -pattern "/api/CashLoadService.svc" -allmatches –simplematch `
+|`
+  ForEach-Object {
+        
+    $parts = $_.Line -split ' '
+
+    $info = @{}
+    $info.Date=$parts[0] 
+    $info.Time =$parts[1]
+    $info.Method = $parts[3]
+    $info.Uri = $parts[4]
+    $info.UriQ = $parts[5]
+    $info.Port = $parts[6]
+    $info.ClientIp = $parts[8]
+    $info.Status = $parts[10]
+    $info.TimeTaken = $parts[13]
+    $object = New-Object -TypeName PSObject –Prop $info
+    
+    $total++
+    
+	<# Count or report the row if the reponse time exceeded 3.875 seconds #>
+	if ([convert]::ToInt32($info.TimeTaken, 10) -gt 3875) 
+    {
+        Write-Output $object
+        $long++
+    }
+
+  } 
+"Total: $total"
+"Long: $long"
+
---
+++ Monitoring/MsmqMonitor.ps1
@@ -0,0 +1,167 @@
+﻿
+<#
+	Sean K. Anderson / seananderson.us / 2014
+	
+	This script is currently set up to run directly on a server hosting Microsoft MSMQ queues.  
+	When it runs it gets the queue depth, and if it exceeds the threshhold number of messages waiting in the queue,
+	it then measures the response time of an API that services the queues.  The response time of the API and the number 
+	of messages waiting in the queues are reported in an email to a support group.
+	
+	I wrote this to warn the team of impending problems related to message queues getting backlogged.  
+	When a backlog would occur we would need to know which queue was backlogged and the reponse time of the API that was 
+	being used to service the queue so that we could start out looking in the right direction.  Before implementing
+	this script there would usually be an hour or two of chaos before we could identify the root cause--and the 
+	queue had already filled the disk.  This got us out in front of the issue while it was still manageable.
+	
+#>
+
+# Set up command line parameters with defaults
+param([int]$messageThreshold=100000, [int]$responseThreshold=2000, [string]$alertEmail="applicationsupport@rushcard.com")
+
+$PSEmailServer = "deliver.contoso.com"
+
+$EtClientId = "ymbq8axy6ngd6e5"
+$EtClientSecret = "ZYGJ8VeQzDTGrx"
+
+$fromEmail = "MSMQ_monitor@contoso.com"
+$toEmail = "prod_support@contoso.com"  #$alertEmail
+
+$emailSubject = "Internal SMS Alerts from $env:computername"
+
+#load this assembly to access json parser in .net if you are stuck with Powershell 2.0 
+[System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions") | Out-Null
+
+Function parseJson($theJson){
+
+    $ser = New-Object System.Web.Script.Serialization.JavaScriptSerializer
+    $ser.DeserializeObject($theJson)
+}
+
+Function getMSMQMessageCount($queueName) {
+    $query = "SELECT MessagesinQueue FROM Win32_PerfRawData_MSMQ_MSMQQueue WHERE Name = '$queueName'"
+    $wmiObject = Get-WmiObject -Query $query
+    $wmiObject.MessagesinQueue
+}
+
+
+Function RestRequest([string]$URL, [string]$Body){
+
+    <# Powershell 3.0+ only #>
+    #$res = Invoke-WebRequest -Uri "https://auth.exacttargetapis.com/v1/requestToken" -Method Post -Body $json -ContentType "application/json"
+    #$obj = parseJson($res)
+    #$obj.accessToken
+
+    <# The PowerShell 2.0 .NET way #>
+    # Convert the message body to a byte array
+    [byte[]]$BodyBytes = [System.Text.Encoding]::UTF8.GetBytes([string]$Body)
+    Write-Host $BodyBytes
+    #Write-Host $BodyBytes.Length
+    $URI = [System.Uri]$([string]$URL)
+    $WebRequest = [System.Net.HttpWebRequest]::CreateHttp([string]$URI)
+    $WebRequest.UserAgent = $("{0} (PowerShell {1}; .NET CLR {2}; {3})" -f "PowerShell", $(if($Host.Version){$Host.Version}else{"1.0"}), [Environment]::Version, [Environment]::OSVersion.ToString().Replace("Microsoft Windows ", "Win"));
+    $WebRequest.Method = 'POST'
+    $WebRequest.ContentType = "application/json"
+    $WebRequest.ContentLength = $BodyBytes.Length
+    $WebRequest.Timeout = 20000;
+    $stream = $WebRequest.GetRequestStream()
+    $stream.Write($Body)
+    $stream.flush();
+    $Stream.Close();
+    
+    try{
+
+        $reader = New-Object System.IO.Streamreader($WebRequest.GetResponse().GetResponseStream())
+        $reader.ReadToEnd()
+        $reader.Close()
+        $WebRequest = $null
+    }
+    catch {
+        
+        Write-Host "Error..."$_.Exception.InnerException -ForegroundColor Yellow
+        Send-MailMessage -to $toEmail -from $fromEmail -Subject $emailSubject -body "ERROR getting response from Exact Target Auth API" -BodyAsHtml
+        exit;
+       
+    }
+    finally {
+        
+        if ($reader){
+            $reader.Close()
+        }
+    }
+}
+
+$messaging = getMSMQMessageCount("cb-nsb\\private$\\contoso.messaging.application");
+$messaging_pri = getMSMQMessageCount("cb-nsb\\private$\\contoso.messaging.application.priority");
+$exactTarget = getMSMQMessageCount("cb-nsb\\private$\\contoso.messaging.sms.exacttarget");
+$exactTarget_pri = getMSMQMessageCount("cb-nsb\\private$\\contoso.messaging.sms.exacttarget.priority");
+
+If ($messaging -gt $messageThreshold -or $messaging_pri -gt $messageThreshold -or $exactTarget -gt $messageThreshold -or $exactTarget_pri -gt $messageThreshold)
+{   
+    $json = '{ "clientId" : "'+$EtClientId+'", "clientSecret" : "'+$EtClientSecret+'" }';
+    $authtime = Measure-Command {$res = RestRequest 'https://auth.exacttargetapis.com/v1/requestToken' $json}
+    try{
+    
+        $obj = parseJson($res)
+        $authtoken = $obj.accessToken
+
+    }catch{
+    
+        Send-MailMessage -to $toEmail -from $fromEmail -Subject $emailSubject -body "ERROR processing Exact Target Auth API reponse" -BodyAsHtml
+        exit;
+    }
+
+    $sendtime = Measure-Command {$res = RestRequest 'https://auth.exacttargetapis.com/v1/requestToken' $json}
+    try{
+    
+        $obj = parseJson($res)
+        # access a property of some json object parseJson($something)
+
+    }catch{
+    
+        Send-MailMessage -to $toEmail -from $fromEmail -Subject $emailSubject -body "ERROR processing Exact Target API reponse" -BodyAsHtml
+        exit;
+    }
+
+    if ($sendtime -gt $responseThreshold){
+
+        $body = "
+        <html> 
+        <h2>The MSMQ queues on $env:computername appear backlogged</h2> 
+        <h3>Exact Target Auth API response time: $authTime milliseconds</h3>
+        <h3>Exact Target SMS API response time: $sendtime milliseconds</h3>
+    
+        <code>
+        <ul>
+        <li>contoso.Messaging Queue Count                         : $messaging </li>
+        <li>contoso.Messaging.Priority Queue Count                : $messaging_pri</li>
+        <li>contoso.Messaging.SMS.ExactTarget Queue Count         : $exactTarget</li>
+        <li>contoso.Messaging.SMS.ExactTarget.Priority Queue Count: $exactTarget_pri</li>
+        </ul>
+        </code>
+    
+        </html>
+        " 
+        
+        Send-MailMessage -to $toEmail -from $fromEmail -Subject $emailSubject -body $body -BodyAsHtml
+
+    }else {
+        Write-host "MSMQ Queue(s) are backlogged but the ET API reponse is less than the threshold of $responseThreshold milliseconds."
+        Write-Host "Exact Target Auth API response time in milliseconds   : $authtime"
+        Write-Host "contoso.Messaging Queue Count                         : $messaging"
+        Write-Host "contoso.Messaging.Priority Queue Count                : $messaging_pri"
+        Write-Hoat "contoso.Messaging.SMS.ExactTarget Queue Count         : $exactTarget"
+        Write-Host "contoso.Messaging.SMS.ExactTarget.Priority Queue Count: $exactTarget__pri"
+    }
+}else {
+    Write-host "No MSMQ Backlog"
+} 
+
+<#
+
+    <add key="ExactTarget_Auth_Url" value="https://auth.exacttargetapis.com/v1" xdt:Transform="Replace(value)" xdt:Locator="Match(key)" />
+    <add key="ExactTarget_Sms_Url" value="https://www.exacttargetapis.com/sms/v1" xdt:Transform="Replace(value)" xdt:Locator="Match(key)" />
+   
+    <add key="ExactTarget_ClientId" value="ymbq8ty9gd6e5" xdt:Transform="Replace(value)" xdt:Locator="Match(key)" />
+    <add key="ExactTarget_ClientSecret" value="ZYGJ8VQzDTGrx" xdt:Transform="Replace(value)" xdt:Locator="Match(key)" />
+
+#>
